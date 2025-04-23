f077345c36f7e0a01d14776da3499c51f3d21729
Nowaxial <Nowaxial@users.noreply.github.com>
2025-04-23 20:18:08 +0200

    Add project files.

---
+++ SmartBook/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SmartBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, SmartBook!");
+        }
+    }
+}
