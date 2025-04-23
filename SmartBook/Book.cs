3a384a0f5f3a6b0b6f2eb4e7d2b0e89db303e308
Nowaxial <Nowaxial@users.noreply.github.com>
2025-04-23 22:41:03 +0200

    Add Book class to manage book details and status
    - Introduces a new `Book` class in the `SmartBook` namespace.
    
    - The class includes properties for title, author, ISBN, and category, along with a borrowing status. It provides methods to borrow and return the book, and overrides `ToString` for formatted output.

---
+++ SmartBook/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartBook
+{
+    public class Book(string title, string author, string isbn, string category)
+    {
+        public string Title { get; private set; } = title;
+        public string Author { get; private set; } = author;
+        public string ISBN { get; private set; } = isbn;
+        public string Category { get; private set; } = category;
+        public bool IsBorrowed { get; private set; } = false;
+
+        public void BorrowBook() => IsBorrowed = true;
+        public void ReturnBook() => IsBorrowed = false;
+
+        public override string ToString()
+        {
+            return $"{Title} av {Author} med ISBN: {ISBN} i kategori : {Category} | Status: [{(IsBorrowed ? "Utlånad" : "Tillgänglig")}]";
+        }
+    }
+}
